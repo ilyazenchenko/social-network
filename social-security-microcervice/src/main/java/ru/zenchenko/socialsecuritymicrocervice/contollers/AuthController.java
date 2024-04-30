02f40658d57e7fa0dc691c0ff263612248da1aa6
Ilya Zenchenko <ilya.zenchenko@icloud.com>
2024-04-30 19:41:32 +0300

    add register fix cors

--- social-security-microcervice/src/main/java/ru/zenchenko/socialsecuritymicrocervice/contollers/AuthController.java
+++ social-security-microcervice/src/main/java/ru/zenchenko/socialsecuritymicrocervice/contollers/AuthController.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -40,6 +42,8 @@ public class AuthController {
 
     private final UserService userService;
 
+    private final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     @GetMapping("/login")
     public ResponseEntity login() {
         if (SecurityContextHolder.getContext().getAuthentication().isAuthenticated()){
@@ -48,9 +52,15 @@ public class AuthController {
         return ResponseEntity.status(403).body(Map.of("message", "unauthenticated"));
     }
 
+    @PostMapping("/register")
+    public String performRegister(@RequestBody User user) {
+        registrationService.register(user);
+        return "redirect:/auth/login";
+    }
+
     @PostMapping("/process_login")
     public ResponseEntity<?> processLogin(@RequestBody LoginDto loginDto) {
-        // Ваш код для проверки логина и пароля
+        logger.info(loginDto.getLogin());
         boolean isAuthenticated = authenticate(loginDto.getLogin(), loginDto.getPassword());
         if (isAuthenticated) {
             User user = ((MyUserDetails) myUserDetailsService.loadUserByUsername(loginDto.getLogin())).getUser();
