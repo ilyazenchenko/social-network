c63ee6a342bccc53a38070431bf92c8058abdb79
Ilya Zenchenko <ilya.zenchenko@icloud.com>
2024-04-30 16:34:36 +0300

    SubController to Rest

--- social-security-microcervice/src/main/java/ru/zenchenko/socialsecuritymicrocervice/contollers/SubscriberController.java
+++ social-security-microcervice/src/main/java/ru/zenchenko/socialsecuritymicrocervice/contollers/SubscriberController.java
@@ -1,34 +1,46 @@
 package ru.zenchenko.socialsecuritymicrocervice.contollers;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import ru.zenchenko.socialsecuritymicrocervice.models.User;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Controller
-@RequestMapping("/{sub}/subscribe")
+@RestController
+@RequestMapping("/subs/{sub}")
 @RequiredArgsConstructor
 public class SubscriberController {
 
     private final RestTemplate restTemplate;
 
+    private final Logger logger = LoggerFactory.getLogger(SubscriberController.class);
+
     @PostMapping("/{sub_to}")
-    public String subscribe(@PathVariable int sub, @PathVariable int sub_to) {
-        Map response = restTemplate.postForObject("http://localhost:8081/" + sub + "/subscribe/" + sub_to,
+    public ResponseEntity<Map> subscribe(@PathVariable int sub, @PathVariable("sub_to") int subTo) {
+        Map response = restTemplate.postForObject("http://localhost:8081/" + sub + "/subscribe/" + subTo,
                 null, Map.class);
-        System.out.println(response);
-        return "redirect:/"+sub_to;
+        Map result = (HashMap) response;
+        result.put("from", sub);
+        result.put("to", subTo);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{sub_to}")
-    public String unsubscribe(@PathVariable int sub, @PathVariable int sub_to) {
-        restTemplate.delete("http://localhost:8081/" + sub + "/subscribe/" + sub_to);
-        System.out.println("sub deleted");
-        return "redirect:/"+sub_to;
+    public ResponseEntity unsubscribe(@PathVariable int sub, @PathVariable("sub_to") int subTo) {
+        restTemplate.delete("http://localhost:8081/" + sub + "/subscribe/" + subTo);
+        logger.info("sub deleted");
+        return ResponseEntity.ok(Map.of(
+                "sub", "deleted",
+                "from", sub,
+                "to", subTo
+        ));
     }
 
 }
